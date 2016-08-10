202930f344487d677d576fb480d5726b07716394
Diogo Dornelles <diogo.s.dornelles@gmail.com>
2016-08-10 12:09:38 -0300

    changed dicesimulator to use a constructor function

--- dice_simulator_2015/dice.js
+++ dice_simulator_2015/dice.js
@@ -1,7 +1,9 @@
-var dice = {
-  sides: 6,
-  roll: function () {
-        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-        return randomNumber;
-        }
+function Dice (sides) {
+  this.sides = sides;
 }
+Dice.prototype.roll = function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+      }
+
+var dice = new Dice(6);
